0948dcd595adb3877725a86d416c9e746ac4c32f
billybillyjim <thereallmwels@gmail.com>
2022-03-04 19:45:01 -0500

    Great Tree now works with progress flag saves

--- Quepland_2_DN6/GreatTree.cs
+++ Quepland_2_DN6/GreatTree.cs
@@ -2,6 +2,27 @@
 
 public static class GreatTree
 {
-	public static double Progress { get; set; }
-	public static double Height { get; set; } = 300;
+	public static int Progress { get; set; }
+	public static int Height { get; set; } = 300;
+	private static bool init = false;
+
+	public static void Climb(int amount)
+    {
+		Progress += amount;
+		GameState.GetFlagByName("Great Tree Climb").ProgressValue = Progress;
+		if(Progress >= Height)
+        {
+			GameState.GetFlagByName("Great Tree Climb").Completed = true;
+
+		}
+    }
+	public static void Initialize()
+    {
+        if (!init)
+        {
+			Progress = GameState.GetFlagByName("Great Tree Climb").ProgressValue;
+
+		}
+		init = true;
+    }
 }
