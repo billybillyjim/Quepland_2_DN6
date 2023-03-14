3ff70de5af65cc9f5e7236a555422a9bc9a9c8f3
billybillyjim <thereallmwels@gmail.com>
2023-03-13 21:24:57 -0500

    Add debug manager

---
+++ Quepland_2_DN6/Managers/DebugManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+
+public class DebugManager
+{
+    private static readonly DebugManager instance = new DebugManager();
+    private DebugManager() { }
+    static DebugManager() { }
+    public static DebugManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    public Dialog newDialog { get; set; }
+    public Dictionary<string, List<Dialog>> dialogs { get; set; } = new Dictionary<string, List<Dialog>>();
+
+    public void AddDialog()
+    {
+        newDialog = new Dialog();
+    }
+}
+
